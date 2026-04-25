29a60bf0f18de2a4b175569939f999efe20f8835
Masato Takatani <takatani.masato@gmail.com>
2026-04-25 03:15:32 -0600

    selection command

--- 01_rpg_battle/src/source.c
+++ 01_rpg_battle/src/source.c
@@ -1,12 +1,19 @@
 /* battle.c */
 /* 2026-04-23 */
+/* console setting
+ * font size = 72
+ * font = misaki
+ * widh 32
+ * hight 10
+ * write in alacritty-game.toml
+ */
 
 /* [1] Headers */
 #include "console_manager.h"
 #include "input_handler.h"
 #include <locale.h>  // for ncurses
 #include <ncurses.h> // printwを使うために必要
-#include <stdio.h>
+// #include <stdio.h>
 
 /* [2] Constants */
 
@@ -16,6 +23,13 @@ enum {
   MONSTER_PLAYER, // player
   MONSTER_SLIME,  // slime
   MONSTER_MAX     // kinds of monster
+};
+
+enum {
+  COMMAND_FIGHT, // [3-3-1] fight
+  COMMAND_SPELL, // [3-3-2] spell
+  COMMAND_RUN,   // [3-3-3] runaway
+  COMMAND_MAX    // [3-3-4] number of commands
 };
 
 // kinds of characters
@@ -30,6 +44,7 @@ typedef struct {
   int maxMP;            // Max MP
   char name[4 * 3 + 1]; // name 4文字 x 全角3バイト(utf8) + '\0'
   char aa[256];         // [4-1-7] ASCII art
+  int command;          // [4-1-8] command
 } CHARACTER;
 
 /* [5] variables */
@@ -42,18 +57,22 @@ CHARACTER monsters[MONSTER_MAX] = {
         15,         // int mp MP
         15,         // int Max HP
         "ゆうしゃ", // char name name
-        ""          // doesn't have aa but declare as ""
+        "",
+        0, // doesn't have aa but declare as ""
     },
 
     // [5-1-8]MONSTER_SLIME
-    {3,          // int hp
-     3,          // int maxHP
-     0,          // int mp MP
-     0,          // int maxHP
-     "スライム", // char name [4 * 3 +1] name
-                 // [5-1-15] char aa[256] アスキーアート
-     "／・Д・＼\n"
-     "～～～～～"},
+    {
+        3,          // int hp
+        3,          // int maxHP
+        0,          // int mp MP
+        0,          // int maxHP
+        "スライム", // char name [4 * 3 +1] name
+                    // [5-1-15] char aa[256] アスキーアート
+        "／・Д・＼\n"
+        "～～～～～",
+        0,
+    },
 };
 
 // array of characters
@@ -64,7 +83,7 @@ void Battle(int _monster);   // battle scene
 void Init(void);             // initialize the game
 void DrawBattleScreen(void); // draw battle sceen
 
-/* main */
+/* [6-6] main */
 int main(void) {
   /* to use Japanese mult byte for ncurses */
   setlocale(LC_ALL, "");
@@ -93,6 +112,7 @@ int main(void) {
   //   }
 
   /* ここにプログラムを書いてみる */
+  // [6-6-3] call battle
   Battle(MONSTER_SLIME);
 
   /* ここまでプログラム */
@@ -104,12 +124,40 @@ int main(void) {
   return 0;
 }
 
-// function implement
+/* [6] function implement */
+// [6-4] Battle
 void Battle(int _monster) {
   // initialize monster's status
   characters[CHARACTER_MONSTER] = monsters[_monster];
+
   DrawBattleScreen();
+
+  // [6-4-5] display messages for battel scene
+  printw("%sが　あらわれた！\n", characters[CHARACTER_MONSTER].name);
+
+  //[6-4-6] wait for keybord input
   _getch();
+
+  //[6-4-7] loop until battle ends
+  while (1) {
+    //[6-4-9] repeat characters
+    for (int i = 0; i < CHARACTER_MAX; i++) {
+      // [6-4-11] separate with chosen commmand
+      switch (characters[i].command) {
+      case COMMAND_FIGHT: // [6-4-12] fight
+                          // // [6-4-13] display message to fight
+        printw("%sの　こうげき！\n", characters[i].name);
+
+        // [6-4-14] wait for keyboard input
+        _getch();
+        break;
+      case COMMAND_SPELL: // [6-4-22] spell
+        break;
+      case COMMAND_RUN: // [6-4-35] runaway
+        break;
+      }
+    }
+  }
 
 } // battle scene
 
